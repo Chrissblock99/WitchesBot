74fae8d9dc6cf89d3d84bc0d01f68c9bab7e2250
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-10-11 00:33:50 +0200

    add tests for packet parsing

---
+++ src/test/kotlin/packet/PacketTest.kt
@@ -0,0 +1,87 @@
+package packet
+
+import me.chriss99.packet.Packet
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class PacketTest {
+    @Test
+    fun register() {
+        val packet = """REGISTER 1.0
+          |name: player1
+        """.trimMargin()
+        assertEquals(packet, Packet.parse(packet).serialize())
+    }
+
+    @Test
+    fun passCards() {
+        val packet = """PASS_CARDS 1.0
+          |cards: BE,Y7,F1
+        """.trimMargin()
+        assertEquals(packet, Packet.parse(packet).serialize())
+    }
+
+    @Test
+    fun playCard() {
+        val packet = """PLAY_CARD 1.0
+          |card: R3
+        """.trimMargin()
+        assertEquals(packet, Packet.parse(packet).serialize())
+    }
+
+    @Test
+    fun initGame() {
+        val packet = """INIT_GAME 1.0
+          |players: player1,player2,player3,player4
+          |totalTime: 235647
+          |timeIncrement: 24
+        """.trimMargin()
+        assertEquals(packet, Packet.parse(packet).serialize())
+    }
+
+    @Test
+    fun initTrick() {
+        val packet = """INIT_TRICK 1.0
+          |beginner: 1
+          |scores: 13,41,33,37
+        """.trimMargin()
+        assertEquals(packet, Packet.parse(packet).serialize())
+    }
+
+    @Test
+    fun setCards() {
+        val packet = """SET_CARDS 1.0
+          |player: 2
+          |cards: R1,GA,F4
+        """.trimMargin()
+        assertEquals(packet, Packet.parse(packet).serialize())
+    }
+
+    @Test
+    fun chooseCards() {
+        val packet = """CHOOSE_CARDS 1.0
+          |timeLeft: 73242
+          |passingOrder: 2,3,0,1
+          |amount: 3
+        """.trimMargin()
+        assertEquals(packet, Packet.parse(packet).serialize())
+    }
+
+    @Test
+    fun yourTurn() {
+        val packet = """YOUR_TURN 1.0
+          |timeLeft: 234452
+          |validMoves: R1,R4,GA,B3,Y3,F4
+        """.trimMargin()
+        assertEquals(packet, Packet.parse(packet).serialize())
+    }
+
+    @Test
+    fun cardPlayed() {
+        val packet = """CARD_PLAYED 1.0
+          |player: 3
+          |card: F2
+        """.trimMargin()
+        assertEquals(packet, Packet.parse(packet).serialize())
+    }
+}
