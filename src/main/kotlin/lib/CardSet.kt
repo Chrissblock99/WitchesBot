ecf7d8aafb3c4726d56cd8c2d8ccf50af5480e6f
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-10-11 22:57:24 +0200

    implement equals and hashCode as well as toString for CardSet

--- src/main/kotlin/lib/CardSet.kt
+++ src/main/kotlin/lib/CardSet.kt
@@ -13,4 +13,19 @@ class CardSet {
     }
 
     operator fun contains(card: Card): Boolean = (cards and card.indexBit) != 0L
+
+    override fun toString() = "CardSet(cards=${cards.toULong().toString(2)})"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CardSet) return false
+
+        if (cards != other.cards) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return cards.hashCode()
+    }
 }
