b4fb12e5e2686f8c5f8302e0417566893581b1d0
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-10-17 20:17:32 +0200

    add utility functions and constants to CardSet

--- src/main/kotlin/lib/CardSet.kt
+++ src/main/kotlin/lib/CardSet.kt
@@ -7,12 +7,18 @@ class CardSet {
 
     val list get() = Card.entries.filter { it in this }
 
-    constructor(cards: String) : this(Packet.parseList(cards, Card::valueOf))
-    constructor(cards: List<Card>) {
-        this.cards = cards.fold(0) { acc, card -> acc or card.indexBit }
+    private constructor(cards: Long) {
+        this.cards = cards
     }
+    constructor(cards: List<Card>) : this(cards.fold(0) { acc, card -> acc or card.indexBit })
+    constructor(cards: String) : this(Packet.parseList(cards, Card::valueOf))
 
     operator fun contains(card: Card): Boolean = (cards and card.indexBit) != 0L
+    operator fun contains(color: Card.Color): Boolean = list.any { it.color == color }
+
+    infix fun union(other: CardSet) = CardSet(this.cards or other.cards)
+
+    fun filter(predicate: (card: Card) -> Boolean) = CardSet(list.filter(predicate))
 
     override fun toString() = "CardSet(cards=${cards.toULong().toString(2)})"
 
@@ -27,5 +33,10 @@ class CardSet {
 
     override fun hashCode(): Int {
         return cards.hashCode()
+    }
+
+    companion object {
+        val ALL_CARDS = CardSet(Card.entries.toList())
+        val NO_CARDS = CardSet(emptyList())
     }
 }
