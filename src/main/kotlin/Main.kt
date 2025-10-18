ed4cf6a8a6ebe8a89ce71cf643adeef40839d6aa
Chriss99 <75170436+Chrissblock99@users.noreply.github.com>
2025-10-19 00:58:48 +0200

    accept player name as program argument

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,9 +5,10 @@ import me.chriss99.packet.PacketHandler
 
 var shouldRun = true
 
-fun main() {
+fun main(args: Array<String>) {
     val packetHandler = PacketHandler()
-    val bot = RandomBot(packetHandler, "Chriss99")
+    val name = if (args.isEmpty()) "Chriss99" else args[0]
+    val bot = RandomBot(packetHandler, name)
 
     while (shouldRun) {
         packetHandler.takeInputLine(readln() + "\n")
